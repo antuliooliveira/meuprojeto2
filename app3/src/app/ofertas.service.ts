c6da1dc6aed4b62046cb4d48722b57bb1e194bac
ANTULIO <antulio.oliveira@serpro.gov.br>
2019-01-28 15:31:28 -0300

    atualizado28012019

---
+++ app3/src/app/ofertas.service.ts
@@ -0,0 +1,68 @@
+import {Oferta} from './shared/oferta.model';
+import { promise } from 'protractor';
+export class OfertasService {
+public ofertas: Array<Oferta> = [
+	{
+		id: 1,
+		categoria: 'restaurante XYZ',
+		titulo: 'Super Burger A',
+		descricao_oferta: 'Rodízio de Mini-hambúrger com opção de entrada.',
+		anunciante: 'Original Burger',
+		valor: 29.90,
+		destaque: true,
+		imagens: [
+			{url: '/assets/ofertas/1/img1.jpg'},
+			{url: '/assets/ofertas/1/img2.jpg'},
+			{url: '/assets/ofertas/1/img3.jpg'},
+			{url: '/assets/ofertas/1/img4.jpg'}
+		]
+	},
+	{
+		id: 2,
+		categoria: 'restaurante ABC',
+		titulo: 'Cozinha Mexicana B',
+		descricao_oferta: 'Almoço ou Jantar com Rodízio Mexicano delicioso.',
+		anunciante: 'Mexicana',
+		valor: 32.90,
+		destaque: true,
+		imagens: [
+			{url: '/assets/ofertas/2/img1.jpg'},
+			{url: '/assets/ofertas/2/img2.jpg'},
+			{url: '/assets/ofertas/2/img3.jpg'},
+			{url: '/assets/ofertas/2/img4.jpg'}
+		]
+
+	},
+	{
+		id: 4,
+		categoria: 'diversao WWW',
+		titulo: 'Estância das águas C',
+		descricao_oferta: 'Diversão garantida com piscinas, trilhas e muito mais.',
+		anunciante: 'Estância das águas',
+		valor: 31.90,
+		destaque: true,
+		imagens: [
+			{url: '/assets/ofertas/3/img1.jpg'},
+			{url: '/assets/ofertas/3/img2.jpg'},
+			{url: '/assets/ofertas/3/img3.jpg'},
+			{url: '/assets/ofertas/3/img4.jpg'},
+			{url: '/assets/ofertas/3/img5.jpg'},
+			{url: '/assets/ofertas/3/img6.jpg'}
+		]
+	}
+]
+
+public getOfertas(): Array<Oferta> {
+    return this.ofertas;
+}
+public getOfertas2(): Promise<Oferta[]> {
+  return new Promise((resolve, reject) => {
+  // algum tipo de processamento, que ao finalizar, chama a função resolve ou reject
+  console.log ('passei por aqui');
+  let deu_certo = false;
+  if (deu_certo) {resolve(this.ofertas)}
+  else           { reject({codigo_erro:404, mensagem_erro:'Servidor não encontrado'} )}
+
+
+
+   })} }
